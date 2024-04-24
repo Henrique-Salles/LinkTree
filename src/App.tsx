da0cb1831178109a40719b00e35e884df8712471
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-24 16:22:30 -0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-  return <></>;
-}
+import { createBrowserRouter } from "react-router-dom";
 
-export default App;
+import { Home } from "./pages/home/home";
+import { Login } from "./pages/login/login";
+import { Admin } from "./pages/admin/admin";
+import { Networks } from "./pages/networks/networks";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  {
+    path: "/admin/social",
+    element: <Networks />,
+  },
+]);
+
+export { router };
