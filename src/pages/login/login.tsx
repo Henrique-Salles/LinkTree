65c629a12e8314ceee401050696a0dc39fa5da8c
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-30 16:03:31 -0300

    input component

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/input/input";
+import { FormEvent, useState } from "react";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log({
+      email: email,
+      password: password,
+    });
+  }
+
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -11,6 +24,32 @@ export function Login() {
           </span>
         </h1>
       </Link>
+
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-xl flex flex-col px-2"
+      >
+        <Input
+          placeholder="Digite o seu E-Mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input
+          placeholder="Digite a sua senha"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          type="submit"
+          className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white"
+        >
+          Acessar
+        </button>
+      </form>
     </div>
   );
 }
