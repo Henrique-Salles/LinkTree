ae1f753e297cbb790dc30b5d8b1ef5fe6a681083
Henrique-Salles <henriquesalles1520@gmail.com>
2024-04-30 13:32:40 -0300

    login logo style

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 export function Login() {
   return (
-    <div>
-      <h1>Página Login</h1>
+    <div className="flex w-full h-screen items-center justify-center flex-col">
+      <Link to="/">
+        <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
+          Link
+          <span className="bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
+            Tree
+          </span>
+        </h1>
+      </Link>
     </div>
   );
 }
