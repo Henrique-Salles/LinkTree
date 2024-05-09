26ebb339d5b29678e43525e6b8f9ff1cef095201
Henrique-Salles <henriquesalles1520@gmail.com>
2024-05-09 17:06:53 -0300

    networks deleted

--- src/pages/networks/networks.tsx
+++
@@ -1,7 +0,0 @@
-export function Networks() {
-  return (
-    <div>
-      <h1>Página Rede Sociais</h1>
-    </div>
-  );
-}
---
+++ src/pages/notfound/notfound.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export function NotFound() {
+  return (
+    <div>
+      <h1>Page Not Found 😥</h1>
+    </div>
+  );
+}
